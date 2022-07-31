f20e9fa8654eb573194e32a6374f7548f951fa76
Samuel Norman <sjnorman15@gmail.com>
2022-07-31 01:09:15 -0600

    Cleaned up names for the DeckModel

--- src/main/java/com/otcg/decks/DeckModel.java
+++ src/main/java/com/otcg/decks/DeckModel.java
@@ -6,41 +6,41 @@ public class DeckModel {
     private int id;
     private String deckName;
     private String deckTcg;
-    private ArrayList deck = new ArrayList();
+    private ArrayList cards = new ArrayList();
 
     public Object[] getInfo() {
         return new Object[]{id, deckName, deckTcg};
     }
 
-    public int getId(){
+    public int getId() {
         return id;
-    }
-
-    public String getDeckName(){
-        return deckName;
-    }
-
-    public String getDeckTcg() {
-        return deckTcg;
-    }
-
-    public ArrayList getCards() {
-        return deck;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
+    public String getDeckName() {
+        return deckName;
+    }
+
     public void setDeckName(String deckName) {
         this.deckName = deckName;
+    }
+
+    public String getDeckTcg() {
+        return deckTcg;
     }
 
     public void setDeckTcg(String deckTcg) {
         this.deckTcg = deckTcg;
     }
 
-    public void setDeck(ArrayList deck) {
-        this.deck = deck;
+    public ArrayList getCards() {
+        return cards;
+    }
+
+    public void setDeck(ArrayList cards) {
+        this.cards = cards;
     }
 }
